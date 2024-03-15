6a71e36e1b0fc5046ba4a7b33a81333333934a6c
SiddiNalgonda <srinivasnolgonda@gmail.com>
2024-03-15 14:42:25 +0530

    commit first

---
+++ demogithub/src/QUESTION1/AddTwointegers.java
@@ -0,0 +1,23 @@
+package QUESTION1;
+import java.util.Scanner;
+public class AddTwointegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first integer:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second integer:");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+
+}
+
+	public static int add(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}}
