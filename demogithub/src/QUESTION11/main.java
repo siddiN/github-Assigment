dadadd7840e8e44f5251527e52fb31156a7ace48
SiddiN <siddinalgonda10@gmail.com>
2024-03-23 14:51:12 +0530

    first commit

---
+++ demogithub/src/QUESTION11/main.java
@@ -0,0 +1,24 @@
+package QUESTION11;
+
+public class main {
+
+
+
+	// Main class
+		public class Main {
+		    public static void main(String[] args) {
+		        // Create an object of TeacherTeaches class
+		        TeacherTeaches teacher = new TeacherTeaches();
+
+		        // Set values for type and name
+		        teacher.type = "Mathematics";
+		        teacher.name = "John Doe";
+
+		        // Call methods
+		        teacher.Teaches();
+		        teacher.Subjects();
+		    }
+		}
+
+
+}
